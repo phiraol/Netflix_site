312fcfc7e9ce163e122e3217a01f96578d47c24c
phiraol <166308089+phiraol@users.noreply.github.com>
2025-07-24 21:56:40 +0300

    banner

--- Netflix_site/src/pages/home/Home.jsx
+++ Netflix_site/src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-
+import Banner from '../../components/Banner/Banner'
 const Home = () => {
   return (
     <>
           <Header></Header>
+          <Banner></Banner>
           <Footer></Footer>
     </>
   )
